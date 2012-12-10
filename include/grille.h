97a8e0f2d22b69241a64a274c35402b262252883
max <nunu@ubuntu.(none)>
2012-12-10 19:59:54 +0100

    Ajout de commentaires
    Indique ce que fait chaque fonction dans grille et liste

--- include/grille.h
+++ include/grille.h
@@ -15,13 +15,28 @@ typedef struct StrGrille
 	int j_fin;
 } Grille;
 
+// Initialise la grille
 Grille * init(int, int);
+
+// Remet tous les marquages à 0
 Grille * resetMarquage(Grille *);
+
+// Tue toutes les cellules de la grille
 Grille * resetGrille(Grille *);
+
+// Genere une grille depuis un fichier
 Grille * lectureFichier(char *);
+
+// Fait evoluer une grille
 Grille * evolution(Grille *);
+
+// Affiche une grille
 void afficherGrille(Grille *);
+
+// Découpe un morceau de la grille contenant un tas de cellules vivantes
 Grille * decoupe(Grille *, int, int);
+
+// Désalloue la grille
 void free_grille(Grille *);
 
 #endif
--- include/liste.h
+++ include/liste.h
@@ -14,16 +14,37 @@ typedef struct StrListe
 
 typedef StrListe Liste;
 
+// Genere une nouvelle liste
 Liste * nouvelleListe();
+
+// Ajoute une grille en tete de liste
 Liste * ajoutTete(Grille *, Liste *);
+
+// Ajoute une grille en queue de liste
 Liste * ajoutQueue(Grille *, Liste *);
+
+// Supprime une grille en tete de liste
 Liste * supprTete(Liste *);
+
+// Supprime une grille en queue de liste
 Liste * supprQueue(Liste *);
+
+// Indique si la liste est vide ou pas
 int listeEstVide(Liste *);
+
+// Retourne la taille de la liste
 int tailleListe(Liste *);
+
+// Affiche les grilles de la liste
 void afficherListe(Liste *);
+
+// Decoupe toute la grille en plusieurs et les places dans une liste
 Liste * parcoursGrille(Grille *);
+
+// Integre une grille dans l'autre aux bonnes coordonnées
 Grille * assembler(Grille *, Grille *);
+
+// Recolle toutes les grilles de la liste dans la grille principale
 Grille * recollageGrille(Grille *, Liste *);
 
 #endif
